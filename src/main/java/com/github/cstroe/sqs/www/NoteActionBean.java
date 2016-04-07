7bc0f74d5c10e9a1d942c74720fb00419a208b1b
Cosmin Stroe <cstroe@users.sf.net>
2016-04-07 08:47:49 -0500

    Removed unused parts.

--- src/main/java/com/github/cstroe/sqs/www/NoteActionBean.java
+++ src/main/java/com/github/cstroe/sqs/www/NoteActionBean.java
@@ -1,15 +1,10 @@
 package com.github.cstroe.sqs.www;
 
-import com.github.cstroe.sqs.dao.NotebookDao;
-import com.github.cstroe.sqs.dao.NoteDao;
-import com.github.cstroe.sqs.model.Note;
 import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.HandlesEvent;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.UrlBinding;
-
-import java.time.LocalDateTime;
 
 @UrlBinding("/app/note/{$event}")
 public class NoteActionBean extends BaseActionBean {
@@ -27,9 +22,5 @@ public class NoteActionBean extends BaseActionBean {
 
     public Resolution create() {
         return new ForwardResolution("/notes/addNote.jsp");
-    }
-
-    public Note getNote() {
-        return new NoteDao(1, "A User", LocalDateTime.now(), "Some Note", "Blah\nBlah\nBlah", new NotebookDao(1, "Ungrouped"));
     }
 }
