6b323b9a5eb3743a9b7498761bd86580a8026ebf
Cosmin Stroe <cstroe@users.sf.net>
2016-04-07 18:42:03 -0500

    Further testing.

--- src/test/scala/com/github/cstroe/sqs/www/NotebookPageIT.scala
+++ src/test/scala/com/github/cstroe/sqs/www/NotebookPageIT.scala
@@ -4,25 +4,70 @@ import org.scalatest._
 import org.scalatest.concurrent.Eventually
 import org.scalatest.selenium.HtmlUnit
 
-class NotebookPageIT extends FunSpec with BeforeAndAfter with ShouldMatchers with HtmlUnit with Eventually {
+class NotebookPageIT extends FunSpec with BeforeAndAfterEach with ShouldMatchers with HtmlUnit with Eventually {
 
-  before {
+  override def beforeEach {
     go to StripesUtil.getUrl(classOf[DbUnitActionBean])
+    eventually {
+      pageTitle shouldBe "View Notes"
+    }
   }
 
   describe("The add notebook page") {
-    it("should create a notebook") {
+    def visitPage = {
       go to StripesUtil.getUrl(classOf[NotebookActionBean])
+
+      eventually {
+        pageTitle shouldBe "Add Notebook"
+      }
+
       click on "notebook.name"
-      textField("notebook.name").value = "My New Notebook"
+    }
+
+    describe("when creating a notebook") {
+      def createNotebook = {
+        visitPage
+        textField("notebook.name").value = "My New Notebook"
+        submit
+
+        eventually {
+          pageTitle shouldBe "View Notes"
+        }
+      }
+
+      it("should show it on the view page") {
+        createNotebook
+
+        val notebookNames = findAll(cssSelector("tbody#notebooksList td")).toList.map(_.text)
+        notebookNames should contain("My New Notebook")
+      }
+
+      it("should not allow the user to create a notebook with the same name") {
+        createNotebook
+        visitPage
+
+        textField("notebook.name").value = "My New Notebook"
+        submit
+
+        eventually {
+          pageTitle shouldBe "Add Notebook"
+        }
+
+        val alert = find(cssSelector("div.alert-warning")).get.text
+        alert should include ("A notebook already exists with that name")
+      }
+    }
+
+    it("should not allow a user to create a notebook with a blank name") {
+      visitPage
       submit
 
       eventually {
-        pageTitle shouldBe "View Notes"
+        pageTitle shouldBe "Add Notebook"
       }
 
-      val notebookNames = findAll(cssSelector("tbody#notebooksList td")).toList.map(_.text)
-      notebookNames should contain ("My New Notebook")
+      val alert = find(cssSelector("div.alert-warning")).get.text
+      alert should include ("Notebook name must be specified")
     }
   }
 }
